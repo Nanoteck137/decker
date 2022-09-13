a01015cfc127e2188d2e4ecaa14dcad6a972f15d
Patrik M. Rosesntröm <patrik.millvik@gmail.com>
2022-09-13 20:58:57 +0200

    Added some notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// NOTE(patrik):
+// Offical Valve devkit client: https://gitlab.steamos.cloud/devkit/steamos-devkit
+// URL: http://machine-ip:32000
+// Magic: 900b919520e4cf601998a71eec318fec
+//   - From: https://gitlab.steamos.cloud/devkit/steamos-devkit/-/blob/main/client/devkit_client/__init__.py
+// Endpoints
+//   - /properties.json
+//   - /login-name
+//   - /register
+//     - Data: SSH_KEY + " " + MAGIC
+
 fn main() {
     println!("Hello, world!");
 }
