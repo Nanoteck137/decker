277021bd6de3dd798e80f9c3b79d970d07627a13
Patrik M. Rosesntröm <patrik.millvik@gmail.com>
2022-09-14 12:21:41 +0200

    Registering the client

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // URL: http://machine-ip:32000
 // Magic: 900b919520e4cf601998a71eec318fec
 //   - From: https://gitlab.steamos.cloud/devkit/steamos-devkit/-/blob/main/client/devkit_client/__init__.py
+//   - NOTE: Maybe not needed
 // Endpoints
 //   - /properties.json
 //   - /login-name
@@ -31,7 +32,7 @@ fn get_public_key(key: &Rsa<Private>) -> String {
     format!("ssh-rsa {} {}\n", pub_key, comment)
 }
 
-fn setup() -> Option<()> {
+fn setup(addr: &str) -> Option<()> {
     let key = openssl::rsa::Rsa::generate(2048).ok()?;
 
     let private_key_path = "decker_devkit_key";
@@ -44,6 +45,32 @@ fn setup() -> Option<()> {
     let mut file = File::create("decker_devkit_key.pub").ok()?;
     file.write(get_public_key(&key).as_bytes()).ok()?;
 
+    let url = format!("http://{}/register", addr);
+    // let res = reqwest::blocking::post(url).unwrap().text().unwrap();
+
+    let mut public_key = get_public_key(&key);
+    // TODO(patrik): We might not need to have the magic value because
+    // registering without it works
+    // public_key.pop();
+    // public_key.push_str(" 900b919520e4cf601998a71eec318fec\n");
+
+    let client = reqwest::blocking::Client::new();
+    let res = client.post(url)
+        .body(public_key)
+        .send()
+            .ok()?;
+
+    if res.status().is_client_error() {
+        let res = res.text().unwrap();
+        let res: Value = serde_json::from_str(&res).unwrap();
+        let has_error = res.get("error").is_some();
+        println!("{} {}", res, has_error);
+    } else if res.status().is_success() {
+        println!("Device Registerd");
+    } else {
+        panic!("Unknown error");
+    }
+
     Some(())
 }
 
@@ -52,20 +79,20 @@ fn get_devkit_key() -> Option<Rsa<Private>> {
 }
 
 fn main() {
-    setup()
-        .expect("Failed to setup device");
-
     let addr = if let Ok(addr) = std::env::var("DEVKIT_ADDR") {
         addr
     } else {
         panic!("DEVKIT_ADDR not set");
     };
 
-    let url = format!("http://{}/properties.json", addr);
-    let res = reqwest::blocking::get(url).unwrap().text().unwrap();
+    setup(&addr)
+        .expect("Failed to setup device");
 
-    let res: Value = serde_json::from_str(&res).unwrap();
-    let has_error = res.get("error").is_some();
-
-    println!("{} {}", res, has_error);
+    // let url = format!("http://{}/properties.json", addr);
+    // let res = reqwest::blocking::get(url).unwrap().text().unwrap();
+    //
+    // let res: Value = serde_json::from_str(&res).unwrap();
+    // let has_error = res.get("error").is_some();
+    //
+    // println!("{} {}", res, has_error);
 }
