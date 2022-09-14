1e85311bfe16122c2aad303ad1b142505a4c5e54
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-09-14 17:04:32 +0200

    Executing commands and checking if the key is already registered

--- src/main.rs
+++ src/main.rs
@@ -38,66 +38,6 @@ struct Args {
     command: String,
 }
 
-// fn get_public_key_comment() -> String {
-//     let user = whoami::username();
-//     let hostname = whoami::hostname();
-//     format!("devkit-client:{}@{}", user, hostname)
-// }
-//
-// fn get_public_key(key: &Rsa<Private>) -> String {
-//     let pub_key = key.public_key_to_der().unwrap();
-//     let pub_key = base64::encode(pub_key);
-//
-//     let comment = get_public_key_comment();
-//     format!("ssh-rsa {} {}\n", pub_key, comment)
-// }
-//
-// fn setup(addr: &str) -> Option<Rsa<Private>> {
-//     let key = openssl::rsa::Rsa::generate(2048).ok()?;
-//
-//     let private_key_path = "decker_devkit_key";
-//     println!("Writing the private key to '{}'", private_key_path);
-//     let mut file = File::create(private_key_path).ok()?;
-//     println!("Hello");
-//     file.write(&key.private_key_to_pem().ok()?).ok()?;
-//     let metadata = file.metadata().ok()?;
-//     let mut permissions = metadata.permissions();
-//     permissions.set_mode(0o400);
-//     file.set_permissions(permissions).ok()?;
-//
-//     let public_key_path = "decker_devkit_key.pub";
-//     println!("Writing the public key to '{}'", public_key_path);
-//     let mut file = File::create("decker_devkit_key.pub").ok()?;
-//     file.write(get_public_key(&key).as_bytes()).ok()?;
-//
-//     let url = format!("http://{}/register", addr);
-//     // let res = reqwest::blocking::post(url).unwrap().text().unwrap();
-//
-//     let mut public_key = get_public_key(&key);
-//     // TODO(patrik): We might not need to have the magic value because
-//     // registering without it works
-//     public_key.pop();
-//     public_key.push_str(" 900b919520e4cf601998a71eec318fec\n");
-//
-//     let client = reqwest::blocking::Client::new();
-//     let res = client.post(url).body(public_key).send().ok()?;
-//
-//     if res.status().is_client_error() {
-//         let res = res.text().unwrap();
-//         let res: Value = serde_json::from_str(&res).unwrap();
-//         let has_error = res.get("error").is_some();
-//         println!("{} {}", res, has_error);
-//     } else if res.status().is_success() {
-//         println!("Device Registerd");
-//     } else {
-//         panic!("Unknown error");
-//     }
-//
-//     Some(key)
-// }
-
-// fn get_ssh_key_path() -> Path {}
-
 fn get_public_key() -> Result<String> {
     let mut file = File::open("decker_devkit_key.pub")
         .map_err(|e| Error::FailedToOpenPublicKeyFile(e))?;
@@ -148,6 +88,27 @@ fn register(addr: &str) -> Result<()> {
     }
 }
 
+fn execute_simple_ssh(addr: &str, cmd: &str) -> std::process::Output {
+    let username = "deck";
+    let host = format!("{}@{}", username, addr);
+    let output = Command::new("ssh")
+        .arg("-oBatchMode=yes")
+        .arg("-i")
+        .arg("decker_devkit_key")
+        .arg(host)
+        .arg(cmd)
+        .output()
+        .expect("Failed to execute ssh");
+
+    output
+}
+
+fn check_if_registered(addr: &str) -> bool {
+    let output = execute_simple_ssh(addr, "ls");
+
+    output.status.success()
+}
+
 fn main() {
     let addr = if let Ok(addr) = std::env::var("DEVKIT_ADDR") {
         addr
@@ -157,26 +118,12 @@ fn main() {
 
     println!("Device Address: {}", addr);
 
-    register(&addr).expect("Failed to register device");
+    println!("Check: {}", check_if_registered(&addr));
+    if !check_if_registered(&addr) {
+        register(&addr).expect("Failed to register device");
+    }
 
-    let username = "deck";
-    let host = format!("{}@{}", username, addr);
-    let output = Command::new("ssh")
-        .arg("-i")
-        .arg("decker_devkit_key")
-        .arg(host)
-        .arg("date")
-        .output()
-        .expect("Failed to execute ssh");
-    println!("Output: {:?}", std::str::from_utf8(&output.stdout));
-    println!("Error: {:?}", std::str::from_utf8(&output.stderr));
-
-    //
-    // let url = format!("http://{}/properties.json", addr);
-    // let res = reqwest::blocking::get(url).unwrap().text().unwrap();
-    //
-    // let res: Value = serde_json::from_str(&res).unwrap();
-    // let has_error = res.get("error").is_some();
-    //
-    // println!("{} {}", res, has_error);
+    let output = execute_simple_ssh(&addr, "date");
+    println!("Output: {:?}", output);
+    execute_simple_ssh(&addr, "ls");
 }
