232ce53808b868d0feba8be3180328c46540f69a
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-09-15 11:48:47 +0200

    Making sure the target directory is there when copying decker_util

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ pushd decker_util
 cargo build --target-dir ../target --release
 popd
 
+mkdir -p target/release
 cp target/x86_64-unknown-linux-musl/release/decker_util target/release
 
 cargo run --release
