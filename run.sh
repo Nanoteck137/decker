1f9a60cc67656d2762f66383c0a4ae116d78f27f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-09-15 11:45:03 +0200

    Switching to using musl

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ pushd decker_util
 cargo build --target-dir ../target --release
 popd
 
-cp target/x86_64-unknown-linux-gnu/release/decker_util target/release
+cp target/x86_64-unknown-linux-musl/release/decker_util target/release
 
 cargo run --release
